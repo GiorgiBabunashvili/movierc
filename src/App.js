4268da1198eb2bf63296db54fe612926d49979a5
shanME <giorgie103188@gmail.com>
2024-10-01 15:12:20 +0400

    i react react-router

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import HomePage from "./home/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
