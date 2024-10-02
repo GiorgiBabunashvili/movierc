32117ff097963c77662f13ed8354e09573885d7d
shanME <giorgie103188@gmail.com>
2024-10-02 18:19:55 +0400

    trending

---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./trending.css";
+
+const Trending = () => {
+  return <div>trending</div>;
+};
+
+export default Trending;
--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Homes from "../components/homes/Homes";
 import Upcomming from "../components/upcomming/Upcomming";
 import { latest, upcome } from "../dummyData";
+import Trending from "../components/trending/Trending";
 
 const HomePage = () => {
   const [items, setItems] = useState(upcome);
@@ -10,6 +11,7 @@ const HomePage = () => {
   return (
     <>
       <Homes />
+      <Trending />
       <Upcomming item={items} title="Upcomming Movies" />
       <Upcomming item={item} title="Latest Movies" />
     </>
